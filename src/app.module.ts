9a224f07c320d3b88a59a22a56d266fa6446a0a6
KenDo204 <kiethuynh3499@gmail.com>
2026-04-24 11:22:13 +0700

    fix bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -56,7 +56,8 @@ import { VnpayModule } from './vnpay/vnpay.module';
         },
         autoLoadEntities: true,
         // Ở chế độ dev, synchronize=true giúp tự tạo bảng. Khi bảo vệ xong đem lên thật thì nên tắt.
-        synchronize: true,
+        // synchronize: true,
+        synchronize: false,
 
         ssl: process.env.NODE_ENV === 'production'
           ? { rejectUnauthorized: false }
